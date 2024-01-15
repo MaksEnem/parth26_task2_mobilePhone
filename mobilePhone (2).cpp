6e9f5ef28210cea395d59c600b5ccb47ab349eae
MaksEnem <110655471+MaksEnem@users.noreply.github.com>
2024-01-15 18:53:11 +1100

    addSubscribesWork

---
+++ mobilePhone (2).cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class AddressBook
+{
+private:
+
+		std::vector <std::string> contactName;
+		std::vector <std::string> numberPhone;
+
+public:
+
+	std::string volContactName;
+	std::string volNumberPhone;
+
+	void SetAddContactName()
+	{
+		std::cout << "Enter the subscriber's name: ";
+		std::cin >> volContactName;
+
+		contactName.push_back(volContactName);
+		
+	}
+
+	void SetAddNumberPhone()
+	{
+
+		int number;
+		std::cout << "Enter the subscriber's phone number(10 digits): ";
+		std::cin >> number;
+
+		volNumberPhone = "+7" + std::to_string(number);
+		
+		numberPhone.push_back(volNumberPhone);
+		
+	}
+};
+
+class Phone
+{
+private:
+
+
+
+
+public:
+
+
+	void Call()
+	{
+
+
+
+	}
+
+
+
+
+};
+int main() {
+
+	AddressBook subscriberBook;
+	Phone communique;
+
+	std::string command;
+	
+
+	do
+	{
+
+		std::cout << "Enter command: ";
+		std::cin >> command;
+
+		if (command == "add")
+		{
+			subscriberBook.SetAddContactName();
+			subscriberBook.SetAddNumberPhone();
+		}
+		else if (command == "call")
+		{
+
+
+
+		}
+		else if (command == "sms")
+		{
+
+
+
+		}
+
+	} while (command != "exit");
+	
+	
+}
